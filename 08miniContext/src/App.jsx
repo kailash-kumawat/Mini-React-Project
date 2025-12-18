83f62ad0ae7f06f9fad9f5d8333e42daf2c17646
kailash kumawat <kumawatkai369@gmail.com>
2025-12-18 12:19:17 +0530

    feat: initialize 08miniContext project with Vite and React setup
    - Add
    - Implement App component with UserContextProvider
    - Create UserContext and UserContextProvider for user state management

---
+++ 08miniContext/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import UserContextProvider from "./Context/UserContextProvider";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <UserContextProvider>
+      <h1>Chai aur react</h1>
+    </UserContextProvider>
+  );
+}
+
+export default App;
